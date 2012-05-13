5cfb1fb4eca471de38213dd68f93d35108312093
cfddream <cfddream@gmail.com>
2012-05-13 22:57:37 +0800

    rename events -> emitter

--- test/emitter.js
+++ test/emitter.js
@@ -1,8 +1,9 @@
-describe('Events', function(){
+describe('Emitter', function(){
   describe('#on()', function(){
     it('开始注册事件', function(){
       console.log('#on');
-      var e = new Events();
+      var e = new Emitter();
+      console.dir(e);
       e.on("change:title change:author", function () {
         console.log(arguments);
       });
@@ -10,11 +11,11 @@ describe('Events', function(){
   })
 })
 
-describe('Events', function(){
+describe('Emitter', function(){
   describe('#once()', function(){
     it('注册一次性事件', function(){
       console.log('#once');
-      var e = new Events();
+      var e = new Emitter();
       /*e.on('all change:tt', function () {
         console.log('all tt', arguments);
       });
@@ -24,51 +25,51 @@ describe('Events', function(){
       });
       var n = 3;
       while(n) {
-        e.trigger('dd change:author', n);
+        e.emit('dd change:author', n);
         n--;
       }
     })
   })
 })
 
-describe('Events', function(){
+describe('Emitter', function(){
   describe('#off()', function(){
     it('删除特定事件', function(){
       console.log('#off')
-      var e = new Events();
+      var e = new Emitter();
       e.on("all change:title change:author", function () {
         console.log(arguments);
       });
       var n = 10;
       while(n--) {
         if (n == 5) e.off('change:title');
-        e.trigger('change:title change:author change:once', n);
+        e.emit('change:title change:author change:once', n);
       }
     })
   })
 })
 
-describe('Events', function(){
-  describe('#trigger()', function(){
+describe('Emitter', function(){
+  describe('#emit()', function(){
     it('触发事件', function(){
-      console.log('#trigger');
-      var e = new Events();
+      console.log('#emit');
+      var e = new Emitter();
       e.on("change:title change:author", function () {
         console.log(arguments[0] + ' ' + arguments[1]);
       });
       var n = 10;
       //while(n--) {
-        e.trigger('change:author change:title', n);
+        e.emit('change:author change:title', n);
       //}
     })
   })
 })
 
-describe('Events', function(){
+describe('Emitter', function(){
   describe('#on("all ...")', function(){
     it('all 订阅所有', function(){
-      console.log('#trigger all');
-      var e = new Events();
+      console.log('#emit all');
+      var e = new Emitter();
       e.on("all post change", function () {
         console.log(arguments);
       });
@@ -81,7 +82,7 @@ describe('Events', function(){
 
       var n = 10;
       //while(n--) {
-      e.trigger('post', n);
+      e.emit('post', n);
       //}
     })
   })
